d991b2ce4f043526cee66539cf798d81b0b00e49
sangeetds <sangeetnarayands@gmail.com>
2022-02-14 19:46:31 +0530

    Cleanup done.

--- app/src/main/java/com/example/remember/LoginViewModel.kt
+++ app/src/main/java/com/example/remember/LoginViewModel.kt
@@ -1,41 +1,20 @@
-package com.socialbox.login.ui
+package com.example.remember
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.remember.LoginResult
-import com.socialbox.login.data.UserRepository
-import com.socialbox.login.data.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val userRepository: UserRepository) :
+class LoginViewModel @Inject constructor() :
   ViewModel() {
 
   private val _loginResult = MutableLiveData<LoginResult>()
   val loginResult: LiveData<LoginResult> = _loginResult
 
-  fun login(user: User) = viewModelScope.launch {
-    Timber.i("Making request to the server for user: $user")
-    val result = userRepository.login(user)
-    _loginResult.value = when (result) {
-      is Result.Success -> LoginResult(success = result.data)
-      is Result.Created -> LoginResult(created = result.data)
-      is Result.Error -> LoginResult(error = result.exception.localizedMessage)
-    }
-  }
-
-  fun updateSettings(user: User) = viewModelScope.launch {
-    Timber.i("Adding name and photo for user: $user")
-    val result = userRepository.updateSettings(user)
-    _loginResult.value = when (result) {
-      is Result.Success -> LoginResult(success = result.data)
-      is Result.Created -> LoginResult(created = result.data)
-      is Result.Error -> LoginResult(error = result.exception.localizedMessage)
-    }
-  }
 }
--- app/src/main/java/com/example/remember/MainActivity.kt
+++ app/src/main/java/com/example/remember/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.remember
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,9 +15,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
 import com.example.remember.ui.theme.RememberTheme
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions.Builder
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
@@ -22,6 +41,102 @@ class MainActivity : ComponentActivity() {
           Greeting("Android")
         }
       }
+    }
+    setUpObserver()
+    darkModeConfigure()
+    setUpButtons()
+    mGoogleSignInClient = googleSignInClient()
+  }
+
+  private val loginViewModel: LoginViewModel by viewModels()
+  private lateinit var googleLoginButton: SignInButton // by lazy { findViewById(id.googleSignInButton) }
+  private lateinit var progressIndicator: CircularProgressIndicator // by lazy { findViewById(id.loading_icon) }
+  private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+  private fun darkModeConfigure() {
+    when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+      Configuration.UI_MODE_NIGHT_YES -> {
+      }
+      Configuration.UI_MODE_NIGHT_NO -> {
+      }
+    }
+  }
+
+  private fun googleSignInClient(): GoogleSignInClient {
+    val gso = Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+      .requestId()
+      .requestEmail()
+      .requestProfile()
+      .build()
+
+    val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    val account = GoogleSignIn.getLastSignedInAccount(this)
+
+    account?.apply {
+      Toast.makeText(this@MainActivity, "Logging in. Please Wait.", Toast.LENGTH_SHORT).show()
+      Timber.i("User $displayName already signed in")
+      progressIndicator.visibility = View.VISIBLE
+    }
+
+    return mGoogleSignInClient
+  }
+
+  private fun setUpObserver() {
+    loginViewModel.loginResult.observe(this@MainActivity, Observer {
+      val loginResult = it ?: return@Observer
+
+      loginResult.apply {
+        error?.let {
+          Timber.e("Login not successful")
+          showLoginFailed(error)
+        }
+        success?.let {
+          Timber.i("Logged in successfully")
+          updateUiWithUser()
+        }
+        setResult(RESULT_OK)
+      }
+    })
+  }
+
+  private fun setUpButtons() {
+    googleLoginButton.setOnClickListener {
+      progressIndicator.visibility = View.VISIBLE
+      Toast.makeText(this@MainActivity, "Logging in. Please Wait.", Toast.LENGTH_SHORT).show()
+      val signInIntent: Intent = mGoogleSignInClient.signInIntent
+      startActivityForResult(signInIntent, 1)
+    }
+  }
+
+  override fun onBackPressed() {
+    super.onBackPressed()
+    finish()
+  }
+
+  private fun updateUiWithUser() {
+  }
+
+  private fun showLoginFailed(errorString: String) {
+    Timber.d("Logging failed.")
+    Toast.makeText(applicationContext, errorString, Toast.LENGTH_LONG).show()
+    progressIndicator.visibility = View.GONE
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == 1) {
+      val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+      handleSignInResult(task)
+    }
+  }
+
+  private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    try {
+      val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+    } catch (e: ApiException) {
+      Timber.e("signInResult:failed code = ${e.statusCode} with message: ${e.message}")
+      showLoginFailed(e.message ?: "Error Connecting to Google")
     }
   }
 }
