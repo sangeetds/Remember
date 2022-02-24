931b6fdcd5a700ff81850fb9a74ef58f574950df
sangeetds <sangeetnarayands@gmail.com>
2022-02-24 16:05:01 +0530

    Tests initiated.

---
+++ app/src/test/java/com/example/remember/UtilsTest.kt
@@ -0,0 +1,13 @@
+package com.example.remember
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UtilsTest {
+
+  @Test
+  fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
+  }
+}
--- app/src/test/java/com/example/remember/events/ui/EventsViewModelTest.kt
+++ app/src/test/java/com/example/remember/events/ui/EventsViewModelTest.kt
@@ -1,5 +1,37 @@
 package com.example.remember.events.ui
 
+import androidx.lifecycle.Observer
+import com.example.remember.TestCoroutineRule
+import com.example.remember.events.EventsResult
+import io.mockk.mockk
+import io.mockk.spyk
 import junit.framework.TestCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
-class EventsViewModelTest : TestCase()
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class EventsViewModelTest : TestCase() {
+
+  @get:Rule
+  val testCoroutineRule = TestCoroutineRule()
+
+  private lateinit var eventsViewModel: EventsViewModel
+  private val loginResultObserver: Observer<EventsResult> = spyk()
+
+  @Before
+  override fun setUp() {
+    eventsViewModel = EventsViewModel()
+  }
+
+  @Test
+  fun `sampleTest`() {
+
+  }
+}
